a3591439418d1a51371430d3be2a27b620bee216
Luiz Carlos <luizzlcs@hotmail.com>
2023-03-01 13:26:19 -0300

    Ajutando o provide no gridView

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/components/produtct_item.dart';
-import 'package:shop/models/product.dart';
-import 'package:shop/models/product_provider.dart';
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductProvider>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -19,18 +13,9 @@ class ProductsOverviewPage extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemBuilder: (context, index) =>
-            ProdutctItem(product: loadedProducts[index]),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 3,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
+
+
